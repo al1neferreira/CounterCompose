dfb18675a073aa501d6809312cdd3c4bca6e5c35
Aline Ferreira <alinfst@gmail.com>
2024-07-19 20:05:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/countercompose/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.countercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.countercompose.ui.theme.CounterComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterComposeTheme {
+                ClickCount()
+            }
+        }
+    }
+}
+
+@Composable
+fun ClickCount() {
+    var count by remember { mutableIntStateOf(value = 0) }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            fontSize = 34.sp,
+            text = count.toString(),
+        )
+        Spacer (modifier = Modifier.size(10.dp))
+
+        Button(onClick = {
+            count += 1
+        }) {
+            Text(text = "Increment")
+        }
+
+    }
+
+}
